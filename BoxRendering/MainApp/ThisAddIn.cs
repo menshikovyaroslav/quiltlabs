afb786350633849589fd4762ef382bcfeb84e931
Ярослав Меньшиков <menshikov.yaroslav@gmail.com>
2023-06-23 11:30:09 +0400

    Main methods to work with Excel

--- BoxRendering/MainApp/ThisAddIn.cs
+++ BoxRendering/MainApp/ThisAddIn.cs
@@ -11,6 +11,42 @@ namespace MainApp
 {
     public partial class ThisAddIn
     {
+        /// <summary>
+        /// Get Application Object
+        /// </summary>
+        /// <returns></returns>
+        public Excel.Application GetApplication()
+        {
+            return Application;
+        }
+
+        /// <summary>
+        /// Get Active Work Book
+        /// </summary>
+        /// <returns></returns>
+        public Excel.Workbook GetActiveWorkBook()
+        {
+            return (Excel.Workbook)Application.ActiveWorkbook;
+        }
+
+        /// <summary>
+        /// Get Active Work Sheet
+        /// </summary>
+        /// <returns></returns>
+        public Excel.Worksheet GetActiveWorksheet()
+        {
+            return (Excel.Worksheet)Application.ActiveSheet;
+        }
+        
+        /// <summary>
+        /// Get Active Cell
+        /// </summary>
+        /// <returns></returns>
+        public Excel.Range GetActiveCell()
+        {
+            return (Excel.Range)Application.Selection;
+        }
+
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
         }
